42de489dd9b28390b09162a5ad5312d00c8b503a
mdshoaib77 <email4966@gmail.com>
2026-04-15 08:04:10 +0600

    Alhamdulliah ts setup

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Assalamuoyalaikum ${person}, Wellcome to my typescript journey`;
+}
+const username = "Alhamdulliah Learning Ts";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greet(person: string): string {
+    return `Assalamuoyalaikum ${person}, Wellcome to my typescript journey`;
+}
+
+const username: string = "Alhamdulliah Learning Ts";
+console.log(greet(username));
